ec4ea8f8e364cf5589719f49b39a5c1e2b517937
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-08 08:43:14 +0100

    add screenshot

--- playground/uitest/README.md
+++ playground/uitest/README.md
@@ -2,6 +2,10 @@
 
 A quick intro to Xcode, Swift and SwiftUI.
 
+<p align="center">
+    <img src="screenshot.png" style="width:528px;max-width:100%;">
+</p>
+
 ## Features
 
 - setting up a Swift Project in Xcode.
--- playground/uitest/uitest/ContentView.swift
+++ playground/uitest/uitest/ContentView.swift
@@ -27,6 +27,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
