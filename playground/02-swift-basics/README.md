5dae2e6eccab86fa0482bd7d8907e2884a5bbe8c
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-08 20:09:59 +0100

    add functions

--- playground/02-swift-basics/README.md
+++ playground/02-swift-basics/README.md
@@ -5,5 +5,6 @@ A quick intro to Swift syntax.
 ## Features
 
 - declaring variables, using constants and understanding data types.
+- using functions with various parameters, return types, argument labels and function signatures.
 
 Based on [SwiftUI Tutorial for Beginners](https://www.youtube.com/watch?v=F2ojC6TNwws) by Chris Ching - CodeWithChris (2021).
--- playground/02-swift-basics/swift-basics.playground/Contents.swift
+++ playground/02-swift-basics/swift-basics.playground/Contents.swift
@@ -1,1 +1,50 @@
-var greeting = "Hello, playground"
+// VARIABLES, CONSTANTS & DATA TYPES
+
+// string
+"hello"
+
+// int
+12
+10
+
+// double
+4.3
+1.2
+
+// bool
+true
+false
+
+var greeting:String = "Hello, playground"
+greeting = "hello again"
+//print(greeting)
+
+var myInt:Int = 100
+//print(myInt)
+
+let myConstant:String = "hello"
+//print(myConstant)
+
+// FUNCTIONS
+
+func myFunc(a:Int, b:Int = 0) -> Int {
+    // let a = 10
+    // let b = 20
+    //print(a+b)
+    return a+b
+}
+
+// let mySum = myFunc(a: 10, b: 20)
+// print(mySum)
+
+// func myFunc2() -> Void {} // returns nothing
+
+// argument labels
+func myFunc3(firstNumber a:Int, secondNumber b:Int = 0) -> Int {
+    // let a = 10
+    // let b = 20
+    //print(a+b)
+    return a+b
+}
+
+print(myFunc3(firstNumber: 2, secondNumber: 2))
