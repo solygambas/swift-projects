e6e1ed2aaf8e2eafb2565320b6d0f30ac91e9fd0
solygambas <51904909+solygambas@users.noreply.github.com>
2022-03-09 15:26:38 +0100

    add swift intro

---
+++ playground/04-swift-intro/README.md
@@ -0,0 +1,10 @@
+# Introduction to Swift
+
+The fundamentals of the Swift programming language.
+
+## Features
+
+- creating variables, constants and strings.
+- storing whole numbers and decimal numbers.
+
+Based on [100 Days of SwiftUI](https://www.hackingwithswift.com/100/swiftui) by Paul Hudson - Hacking with Swift (2022).
---
+++ playground/04-swift-intro/intro.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Cocoa
+
+// STRINGS
+
+var greeting = "Hello, playground"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let filename = "paris.jpg"
+let result = "You win!"
+let quote = "He taped a sign saying \"Believe\" and walked away"
+
+let nameLength = greeting.count
+// print(nameLength) // 17
+
+// print(result.uppercased()) // YOU WIN!
+
+// print(movie.hasPrefix("A day")) // true
+// print(filename.hasSuffix(".jpg")) // true
+
+// NUMBERS
+
+let reallyBig = 100_000_000
+let number = 120
+//print(number.isMultiple(of: 3))
