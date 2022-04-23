3523bc0e301eed67dafbfc190c1e11e78bea17f6
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-23 10:13:07 +0200

    add generating random pizza

--- playground/05-random-pizza/Pizza.playground/Contents.swift
+++ playground/05-random-pizza/Pizza.playground/Contents.swift
@@ -17,4 +17,30 @@
 //let myPizza = Pizza()
 //myPizza.showName()
 
+var ingredients = [
+    "Pepperoni", "Mozzarella", "Bacon", "Sausage", "Basil", "Garlic", "Onion", "Oregano",
+    "Mushromm", "Tomato", "Red Pepper", "Ham", "Chicken", "Red Onion", "Black Olives",
+    "Bell Pepper", "Pineapple", "Canadian Bacon", "Salami", "Jalapeño", "Spinach",
+    "Italian Sausage", "Provolone", "Pesto", "Sun-Dried Tomato", "Feta", "Meatballs",
+    "Prosciutto", "Cherry Tomato", "Pulled Pork", "Chorizo", "Anchovy", "Capers"
+]
+var alreadyChosenIngredients = Array<String>()
 
+//print(ingredients[7]) // oregano
+
+//print(ingredients.randomElement()!)
+//print(ingredients.last!)
+//ingredients.append("Banana")
+//print(ingredients.last!)
+
+func generateRandomIngredient() -> String {
+    print(alreadyChosenIngredients)
+    let newIngredient = ingredients.randomElement()!
+    if (alreadyChosenIngredients.contains(newIngredient)) {
+        return generateRandomIngredient()
+    }
+    alreadyChosenIngredients.append(newIngredient)
+    return newIngredient
+}
+
+print("\(generateRandomIngredient()), \(generateRandomIngredient()), \(generateRandomIngredient()), and \(generateRandomIngredient())")
