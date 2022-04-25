737460ddda04405a3012c4e93bcf4dd748b7f603
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-25 07:41:34 +0200

    add closures

---
+++ playground/09-closures/Closures.playground/Contents.swift
@@ -0,0 +1,74 @@
+// closures
+
+//var closureMessage = {
+//    print("Closures are pretty similar to functions!")
+//}
+//let bookTitle = {
+//    print("Head First Kotlin")
+//}
+
+//let multiplyNumbers = { (number1: Int, number2: Int) in
+//    return "The result is \(number1 * number2)"
+//}
+//
+//print(multiplyNumbers(10, 50))
+
+//let cheeseyPizza = {
+//    print("One Cheesey pizza, coming up!")
+//    print("Cheesey pizza is just cheese, more cheese, and more cheese.")
+//}
+//
+//let plainPizza = {
+//    print("One Plain pizza, coming up!")
+//    print("Cheesey pizza is just cheese, more cheese, and more cheese.")
+//}
+//
+//func order(pizza: () -> Void) {
+//    print("# Ready to order pizza! #")
+//    pizza()
+//    print("# Order for pizza placed! #")
+//}
+//
+//order(pizza: cheeseyPizza)
+//order(pizza: plainPizza)
+
+//let neverGonnaGiveYouUp = { (medium: String) -> String in
+//    "'Never gonna give you up' on \(medium)"
+//}
+//let byeByeBye = { (medium: String) -> String in
+//    "'Bye bye bye' on \(medium)"
+//}
+//
+//func performWithPiano(song: (String) -> String) {
+//    let performance = song("Piano")
+//    print(performance)
+//}
+//
+//performWithPiano(song: neverGonnaGiveYouUp)
+//performWithPiano(song: byeByeBye)
+
+func operateOn(_ array: [Int], operation: (Int, Int) -> Int) -> Int {
+    var cur = array[0]
+    for item in array[1...] {
+        cur = operation(cur, item)
+    }
+    return cur
+}
+
+let numbers = [7, 14, 6, 1, 8]
+
+let test = operateOn(numbers, operation: {(total: Int, next: Int) in
+    return total * next
+})
+let sumResult = operateOn(numbers, operation: +)
+
+let sumClosure = {(total: Int, next: Int) in
+    return total + next
+}
+
+let sumTest = operateOn(numbers, operation: sumClosure)
+
+print(test) // 4784
+print(sumResult) // 36
+print(sumTest) // 36
+
---
+++ playground/09-closures/README.md
@@ -0,0 +1,10 @@
+# Closures
+
+A Swift Playground to learn how to work with closures.
+
+## Features
+
+- creating a function and assigning it to a variable.
+- capturing values from the enclosing scope.
+
+Based on [Head First Swift](https://www.amazon.com/Head-First-Swift-Anthony-Gray/dp/1491922850) by Jon Manning and Paris Buttfield-Addison (2021).
