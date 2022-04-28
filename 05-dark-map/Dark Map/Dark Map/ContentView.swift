772879ef2c97936cdfb27b367033ed8de130e311
solygambas <51904909+solygambas@users.noreply.github.com>
2022-04-28 12:03:18 +0200

    fix alignment

--- 05-dark-map/Dark Map/Dark Map/ContentView.swift
+++ 05-dark-map/Dark Map/Dark Map/ContentView.swift
@@ -17,30 +17,32 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("New Delhi")
-                .font(.system(size: 40))
-                                .fontWeight(.heavy)
-                                .foregroundColor(.primary)
-                .padding()
-            HStack (alignment: .top, spacing: 20) {
-                                Image(systemName: "globe.asia.australia.fill").resizable()
-                                    .frame(width: 40, height: 40)
-                                    .foregroundColor(Color(UIColor.systemIndigo))
-                                VStack (alignment: .leading, spacing: 4) {
-                                    Text("The Best New Delhi Tours")
-                                        .font(.headline)
-                                        .bold()
-                                    Text("Explore the sights of Old and New Delhi.")
-                                        .font(.subheadline)
+            VStack (alignment: .leading) {
+                Text("New Delhi")
+                    .font(.system(size: 40))
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.primary)
+                HStack (alignment: .top, spacing: 20) {
+                                    Image(systemName: "globe.asia.australia.fill").resizable()
+                                        .frame(width: 40, height: 40)
+                                        .foregroundColor(Color(UIColor.systemIndigo))
+                                    VStack (alignment: .leading, spacing: 4) {
+                                        Text("The Best New Delhi Tours")
+                                            .font(.headline)
+                                            .bold()
+                                        Text("Explore the sights of Old and New Delhi.")
+                                            .font(.subheadline)
+                                    }
                                 }
-                            }
+            }.padding()
             Map(coordinateRegion: $region)
                 .edgesIgnoringSafeArea(.all)
             Button(action: {}) {
                             Text("Book a tour").foregroundColor(.white).bold()
-                        }.frame(width: 350, height: 60)
+            }.frame(width: 350, height: 60)
                             .background(Color(UIColor.systemIndigo))
                             .cornerRadius(12)
+                            .padding()
         }
     }
 }
