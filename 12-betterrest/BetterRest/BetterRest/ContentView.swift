2cf134593f5c73423554d5a0c2558a53b367c6d3
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-04 09:59:23 +0200

    add screenshot

--- 12-betterrest/BetterRest/BetterRest/ContentView.swift
+++ 12-betterrest/BetterRest/BetterRest/ContentView.swift
@@ -10,7 +10,14 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     @State private var alertTitle = ""
@@ -19,17 +26,23 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours",
-                        value: $sleepAmount, in: 4...12, step: 0.25)
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours",
+                            value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
             }
             .alert(alertTitle, isPresented: $showingAlert) {
                 Button("OK") {}
--- 12-betterrest/README.md
+++ 12-betterrest/README.md
@@ -2,9 +2,9 @@
 
 An app to help coffee drinkers sleep well, using Core ML.
 
-<!-- <p align="center">
+<p align="center">
     <img src="screenshot.png" style="width:528px;max-width:100%;">
-</p> -->
+</p>
 
 ## Features
 
