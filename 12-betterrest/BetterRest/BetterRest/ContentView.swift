0b4ed16b4699e4d945f1bdf811e8c422b2a74a8a
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-04 09:28:40 +0200

    add core ML

--- 12-betterrest/BetterRest/BetterRest/ContentView.swift
+++ 12-betterrest/BetterRest/BetterRest/ContentView.swift
@@ -6,20 +6,62 @@
 // Copyright © 2022 Solygambas. All rights reserved.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
-        // stepper
-//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        // date
-//        DatePicker("Please enter a date", selection: $wakeUp,
-//                   displayedComponents: .hourAndMinute,
-//                   in: Date.now...).labelsHidden()
-        Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
+            .navigationTitle("BetterRest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedtime)
+                }
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(
+                wake: Double(hour + minute),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Sorry"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
---
+++ 12-betterrest/BetterRest/BetterRest/TestView.swift
@@ -0,0 +1,32 @@
+//
+//
+// TestView.swift
+// BetterRest
+//
+// Created by Solygambas on 04/05/2022
+// Copyright © 2022 Solygambas. All rights reserved.
+//
+        
+
+//import SwiftUI
+//
+//struct TestView: View {
+//    @State private var sleepAmount = 8.0
+//    @State private var wakeUp = Date.now
+//    
+//    var body: some View {
+//        // stepper
+////        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//        // date
+////        DatePicker("Please enter a date", selection: $wakeUp,
+////                   displayedComponents: .hourAndMinute,
+////                   in: Date.now...).labelsHidden()
+//        Text(Date.now.formatted(date: .long, time: .shortened))
+//    }
+//}
+//
+//struct TestView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TestView()
+//    }
+//}
