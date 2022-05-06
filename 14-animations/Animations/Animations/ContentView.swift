7391f306b79a703dd3eea5baf02b249745b98141
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-06 08:29:41 +0200

    add pulse effect

--- 14-animations/Animations/Animations/ContentView.swift
+++ 14-animations/Animations/Animations/ContentView.swift
@@ -12,17 +12,43 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
+        
         // implicit animations
+        
         Button("Tap Me") {
-            animationAmount += 1
+            //animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+//        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+        // .animation(.default, value: animationAmount)
+        
+        // custom animations
+        
+//        .animation(.easeOut, value: animationAmount)
+//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+        // .animation(.easeInOut(duration: 2), value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2).delay(1), value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 1).repeatCount(3, autoreverses: true), value: animationAmount)
+//        .animation(
+//            .easeInOut(duration: 1).repeatForever(autoreverses: true), value: animationAmount)
+        
+        // pulse effect
+        .overlay(
+        Circle()
+            .stroke(.red)
+            .scaleEffect(animationAmount)
+            .opacity(2 - animationAmount)
+            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
