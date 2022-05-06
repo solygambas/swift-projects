371ff10c7760ad3759dfc8d23365373a786a32f8
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-06 08:47:21 +0200

    add rotate effect

--- 14-animations/Animations/Animations/ContentView.swift
+++ 14-animations/Animations/Animations/ContentView.swift
@@ -9,19 +9,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+//    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
         
         // implicit animations
         
-        Button("Tap Me") {
-            //animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
+//        Button("Tap Me") {
+//            //animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
 //        .scaleEffect(animationAmount)
 //        .blur(radius: (animationAmount - 1) * 3)
         // .animation(.default, value: animationAmount)
@@ -39,16 +40,48 @@ struct ContentView: View {
 //            .easeInOut(duration: 1).repeatForever(autoreverses: true), value: animationAmount)
         
         // pulse effect
-        .overlay(
-        Circle()
-            .stroke(.red)
-            .scaleEffect(animationAmount)
-            .opacity(2 - animationAmount)
-            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
+//        .overlay(
+//        Circle()
+//            .stroke(.red)
+//            .scaleEffect(animationAmount)
+//            .opacity(2 - animationAmount)
+//            .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
+//        )
+//        .onAppear {
+//            animationAmount = 2
+//        }
+        
+        // animate bindings
+//        print(animationAmount)
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                .easeInOut(duration: 1)
+//                .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//            Spacer()
+//            Button("Tap me") {
+//                animationAmount += 1
+//            }
+//            .padding(50)
+//                    .background(.red)
+//                    .foregroundColor(.white)
+//                    .clipShape(Circle())
+//                    .scaleEffect(animationAmount)
+      //}
+        
+        // explicit animations
+        
+        // flipping coin
+        Button("Tap me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
         }
+        .padding(50)
+                .background(.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
