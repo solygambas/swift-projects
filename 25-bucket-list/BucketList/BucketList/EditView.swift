498b1bb98953d2e92f59c27b29a47d847400a308
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-22 08:36:10 +0200

    add Wikipedia results

--- 25-bucket-list/BucketList/BucketList/EditView.swift
+++ 25-bucket-list/BucketList/BucketList/EditView.swift
@@ -16,6 +16,12 @@ struct EditView: View {
     @State private var name: String
     @State private var description: String
     var onSave: (Location) -> Void
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
     
     var body: some View {
         NavigationView {
@@ -23,6 +29,22 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading...")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -36,6 +58,9 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     
@@ -45,6 +70,22 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            pages = items.query.pages.values.sorted()
+            loadingState = .loaded
+        } catch {
+            loadingState = .failed
+        }
     }
 }
 
