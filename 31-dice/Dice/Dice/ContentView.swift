a5dd313bef6af5d69fdd64cf24fdd2a68759de55
solygambas <51904909+solygambas@users.noreply.github.com>
2022-05-27 16:01:32 +0200

    add dice

--- 31-dice/Dice/Dice/ContentView.swift
+++ 31-dice/Dice/Dice/ContentView.swift
@@ -14,32 +14,38 @@ struct Result: Identifiable {
 }
 
 struct ContentView: View {
-    @State private var diceNumber = "1"
-    @State private var sideNumber = "6"
+    @State private var diceNumber = 1
+    @State private var sideNumber = 6
     @State private var total = 0
     @State private var results = [Result]()
+    let sidesType = [4, 6, 8, 10, 12, 20, 100]
     
     var body: some View {
         // settings
         Form {
             Section {
-                TextField("Number of Dice", text: $diceNumber)
+                Picker("Number of Dice", selection: $diceNumber) {
+                    ForEach(1..<5) {
+                        Text("\($0)").tag($0)
+                                        }
+                                    }.pickerStyle(.segmented)
                   
             } header: {
                 Text("How many dice?")
             }
             Section {
-                TextField("Number of Sides", text: $sideNumber)
+                Picker("Number of Sides", selection: $sideNumber) {
+                    ForEach(sidesType, id: \.self) {
+                        Text("\($0)")
+                                        }
+                                    }.pickerStyle(.segmented)
             } header: {
-                Text("What type?")
+                Text("How many sides?")
             }
             Section {
                 Button("Roll") {
-                    let dice = Int(diceNumber) ?? 1
-                    let sides = Int(sideNumber) ?? 6
-                   
-                    for _ in 1...dice {
-                        let result = Result(value: Int.random(in: 1...sides))
+                    for _ in 1...diceNumber {
+                        let result = Result(value: Int.random(in: 1...sideNumber))
                         results.insert(result, at: 0)
                         total += result.value
                     }
@@ -48,7 +54,16 @@ struct ContentView: View {
                     ScrollView(.horizontal) {
                         LazyHGrid(rows: [GridItem(.adaptive(minimum: 50))], spacing: 10) {
                     ForEach(results) { result in
-                        Text(String(result.value))
+                        if result.value <= 6 {
+                            Image(systemName: "die.face.\(result.value)")
+                                .font(.title)
+                        } else if result.value <= 50 {
+                                        Image(systemName: "\(result.value).square")
+                                            .font(.title)
+                                    } else {
+                                        Text("\(result.value)")
+                                            .font(.title)
+                                    }
                     }
                     }
                     }
